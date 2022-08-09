08b3c5838eef46590ef7df2865b5a835ee959d1c
John Kim <querobinesj@gmail.com>
2022-08-09 23:11:13 +0800

    feat: adds services card

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#fafafa",
+          200: "#f5f5f5",
+          300: "#eeeeee",
+          400: "#e0e0e0",
+          500: "#bdbdbd",
+          600: "#9e9e9e",
+          700: "#757575",
+          800: "#616161",
+          900: "#424242",
+        },
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/typography"),
