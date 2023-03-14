e48fca1e99350c4cb9c30267c4fc744b0fc3ca6e
John Kim <querobinesj@gmail.com>
2023-03-14 13:48:50 +0800

    change some css styles

--- components/ui/Container.tsx
+++ components/ui/Container.tsx
@@ -5,7 +5,7 @@ interface IContainer {
 
 const Container = ({ children, className }: IContainer) => {
   return (
-    <div className={`max-w-7xl px-4 mx-auto ${className}`}>{children}</div>
+    <div className={`max-w-5xl px-4 mx-auto ${className}`}>{children}</div>
   );
 };
 export default Container;
