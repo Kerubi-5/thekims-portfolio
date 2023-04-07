0ffd50235f583d437c800c1a70d0a11b82761553
John Kim <querobinesj@gmail.com>
2023-04-07 16:56:25 +0800

    creates a list component

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ type WorkPlaces = {
 const Home = () => {
   const Section = ({ title, description, children }: Section) => {
     return (
-      <section className="flex flex-col justify-center items-center mb-10 lg:mt-5">
+      <section className="flex flex-col justify-center items-center mb-10 lg:mt-5 max-w-2xl m-auto">
         <div className="sm:py-2 md:py-5 text-center">
           <h2 className="hover:text-gradient">{title}</h2>
           <p className="text-lg text-gray-500">{description}</p>
@@ -38,12 +38,19 @@ const Home = () => {
       <article>
         <div className="mb-5">
           <h3 className="text-lg dark:text-zinc-200">
-            {title} <span className="text-purple-400">@ {company}</span>
+            {title}{" "}
+            <span
+              title="Company"
+              aria-label="Company"
+              className="text-purple-400"
+            >
+              @{company}
+            </span>
           </h3>
           <small className="text-zinc-300">{date}</small>
         </div>
 
-        <ul className="description">
+        <ul className="list">
           {description.map((desc, idx) => (
             <li className="text-zinc-400" key={idx}>
               {desc}
@@ -56,13 +63,32 @@ const Home = () => {
 
   const workPlaces: WorkPlaces[] = [
     {
+      title: "Web Developer Mentor",
+      company: "Refocus",
+      date: "Jan 2023 - Present",
+      description: [
+        "Working Remotely as a Web Developer Mentor specializing in HTML, CSS, Javascript and React, here I am able to use the skills and experiences I was able to learn throughout my web development career",
+        "Participate in product development and provide product development services",
+        "Provide student counseling services via chat in Intercom",
+      ],
+    },
+    {
+      title: "Fullstack Web Developer",
+      company: "OnlineJobsPH",
+      date: "Mar 2022 - Jan 2023",
+      description: [
+        "Designed and developed a News Website using Next.js and React Emotion with Contentful as the headless CMS for a JAMStack setup architecture for the website",
+        "Also created a prototype for a charity website using the same stack used in the previous project",
+      ],
+    },
+    {
       title: "Frontend Developer",
       company: "Pragtechnologies Corp",
-      date: "Aug - July 2022",
+      date: "Aug 2022 - July 2022",
       description: [
         "Trained in using Next.js, React Typescript, Tailwind, Graphql and Shopify",
         "Developed a Next.js project with Typescript and Shopify GraphQL together as a primary requirement to finish the internship programme",
-        "Collaborated with a team of 4 to create the prototype pragtech website using Next.js with Typescript and Gitlab as a source control for the team",
+        "Collaborated with a team of 4 to create the prototype of the pragtech website using Next.js with Typescript and Gitlab as a source control for the team",
       ],
     },
   ];
@@ -114,7 +140,9 @@ const Home = () => {
         </div>
       </Section>
       <Section title="Where I've worked" description="Places I've worked on">
-        {workPlaces.map(RenderWorkPlaces)}
+        <div className="workExperiences space-y-5 mt-5">
+          {workPlaces.map(RenderWorkPlaces)}
+        </div>
       </Section>
     </Container>
   );
