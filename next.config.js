ad4caaa59b6ec3448aa4232d8f8ce4f3879b7945
John Kim <querobinesj@gmail.com>
2023-04-12 13:32:43 +0800

    adds lqip loader

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
-const withOptimizedImages = require('next-optimized-images');
+const withOptimizedImages = require("next-optimized-images");
 
 const nextConfig = withOptimizedImages({
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["images.ctfassets.net"],
+    format: ["image/avif", "image/webp"],
   },
 });
 
